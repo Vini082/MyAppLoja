1ea7680986e8097a23cabe8c6a886a7de05455ec
Vini082 <viniciussantos082@gmail.com>
2025-08-15 20:21:45 -0300

    Aplicando lista

--- app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
@@ -1,14 +1,24 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaProdutosActivity extends AppCompatActivity {
+
+    RecyclerView idListaProdutos;
+    List<Produtos> lstProdutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,31 @@ public class ListaProdutosActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Criando uma array que vai carregar a lista de produtos
+
+        lstProdutos = new ArrayList<>();
+        lstProdutos.add(new Produtos("A56",R.drawable.a56));
+        lstProdutos.add(new Produtos("S24",R.drawable.s24azul));
+        lstProdutos.add(new Produtos("PS5",R.drawable.ps5));
+        lstProdutos.add(new Produtos("Xbox",R.drawable.xbox));
+        lstProdutos.add(new Produtos("Book 4",R.drawable.book4));
+        lstProdutos.add(new Produtos("Dell",R.drawable.dellnote));
+        lstProdutos.add(new Produtos("Apple Watch",R.drawable.applewatch));
+        lstProdutos.add(new Produtos("Watch 7",R.drawable.watch7));
+        lstProdutos.add(new Produtos("Buds 3 Pro",R.drawable.buds3pro));
+        lstProdutos.add(new Produtos("Air Dots",R.drawable.airdots));
+
+
+        idListaProdutos = findViewById(R.id.idListaProdutos);
+
+        // tipo de layout
+        idListaProdutos.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+
+        //instanciar o adaptador
+        AdaptadorProduto adapter = new AdaptadorProduto(getApplicationContext(),lstProdutos);
+
+        idListaProdutos.setAdapter(adapter);
+
     }
 }
